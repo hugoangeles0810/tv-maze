06493f1333d8916dfca5e26dd1155ccaf84f9dd4
Hugo Angeles <hugoangeles0810@gmail.com>
2016-02-07 11:56:57 -0500

    implementando shows

--- test/client-test.js
+++ test/client-test.js
@@ -1,7 +1,9 @@
 var test = require('tape')
+var nock = require('nock')
 var tvmaze = require('../')
-
 var Client = require('../lib/client')
+
+var endpoint = 'http://api.tvmaze.test'
 
 test('should create a client', function (t) {
   t.ok(tvmaze.createClient, 'should exist')
@@ -10,4 +12,20 @@ test('should create a client', function (t) {
   var client = tvmaze.createClient()
   t.ok(client instanceof Client, 'should be instance of client')
   t.end()
+})
+
+test('should list shows', function (t) {
+  var client = tvmaze.createClient({endpoint: endpoint})
+
+  t.equals(typeof client.shows, 'function', 'should be a function')
+
+  nock(endpoint)
+    .get('/shows')
+    .reply(200, [])
+
+  client.shows(function (err, shows) {
+    t.error(err, 'should be not an error')
+    t.ok(Array.isArray(shows), 'should be an array')
+    t.end()
+  })
 })
